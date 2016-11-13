9e66b35d8cd80eefc6bf22abaad2bd5d461a4034
李暁賓 <newblifes@hotmail.com>
2016-11-13 21:59:05 +0900

    id

--- MyOutlook/MyOutlook/Constants/Constants.cs
+++ MyOutlook/MyOutlook/Constants/Constants.cs
@@ -11,11 +11,11 @@ namespace MyOutlook
 {
     public static class Constants
     {
-        public static readonly string APPLICATION_ID = "d76e0b07-a40d-4901-ac32-7185bb1185a9";
+        public static readonly string APPLICATION_ID = "{your applicationID}";
         public static readonly string REDIRECT_URL = "http://localhost";
         public static readonly string[] Scopes = { "Mail.Read", "Mail.ReadWrite" };
 
         // 邮件一览API
-        public const string MESSAGES_LIST = "https://graph.microsoft.com/v1.0/me/messages?skip=20";
+        public const string MESSAGES_LIST = "https://graph.microsoft.com/v1.0/me/messages";
     }
 }
